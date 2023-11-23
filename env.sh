46d5e075306b25f99df7cb495e1b55e50942aa09
juanm4 <juanm4morales@gmail.com>
2023-11-22 21:18:54 -0300

    .

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MYSQL_HOST=localhost
+export MYSQL_USERNAME=juanm4
+export MYSQL_PASSWORD=bass2347
+
+echo "Variables de entorno configuradas:"
+echo "MYSQL_HOST=$MYSQL_HOST"
+echo "MYSQL_USERNAME=$MYSQL_USERNAME"
+echo "MYSQL_PASSWORD=$MYSQL_PASSWORD"
