76187a322ec2900e816b56e08b2c8972eddb039b
juanm4 <juanm4morales@gmail.com>
2023-11-25 20:00:45 -0300

    Fix Validación Legajo: añadido ConstraintValidator para el legajo.

--- src/main/java/com/challenge/cursos/models/Alumno.java
+++ src/main/java/com/challenge/cursos/models/Alumno.java
@@ -1,23 +1,21 @@
 package com.challenge.cursos.models;
 
 
+import com.challenge.cursos.validators.LegajoUnico;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+
 import jakarta.validation.constraints.NotNull;
 
 
 @Entity
-@Table(name = "alumnos",
-uniqueConstraints = {
-    @UniqueConstraint(
-        name="restriccion_legajo",
-        columnNames = "legajo"
-    )
-})
+@Table(name = "alumnos")
 public class Alumno extends Usuario {
     @NotNull(message = "El legajo no debe ser nulo")
-    
+    @Column(unique = true) @LegajoUnico
+
     private Integer legajo;
 
     public Integer getLegajo() {
